4e42716eb574363923bb72e49e0b3cb1a9bc728b
sonigaurav119 <sonigaurav119@gmail.com>
2021-05-16 19:15:43 +0200

    initial commit

---
+++ apps/readily-web/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'readily-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'readily-web';
+}
